8572a19446b7ce204b280ba7f514a976108c7b4c
Administrator <zjb@aiton.com.cn>
2016-08-08 08:39:01 +0800

    Initial commit

---
+++ app/src/main/java/bmcx/aiton/com/ex01_1/MainActivity.java
@@ -0,0 +1,15 @@
+package bmcx.aiton.com.ex01_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
